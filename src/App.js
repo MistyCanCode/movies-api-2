bb346792ec2b113466336129e6ccc48c2e3198c7
Misty Tomlin (OSV) <misty.tomlin.osv@fedex.com>
2022-07-14 09:33:18 -0500

    Initialize movies api project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React, {useState, useEffect} from 'react'
 
 function App() {
+
+  const [endPoint, setEndPoints] = useState('')
+
+  const [container, setContainer] = useState([])
+
+ useEffect(() => {
+  fetchMe()
+ }, [endPoint])
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'f0ac5ede86msh263ca967f67a09dp12b34bjsnd5a50deecddb',
+      'X-RapidAPI-Host': 'online-movie-database.p.rapidapi.com'
+    }
+  };
+  
+  const fetchMe = () => {
+    fetch(`https://online-movie-database.p.rapidapi.com/auto-complete?q=${endPoint}`, options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .then(data => setContainer(data))
+    .catch(err => console.error(err));
+  }
+
+    const onChangeHandler = (e) => {
+      setEndPoints(e.target.value)
+    }
+
+    const submitHandler = e => {
+      e.preventDefault()
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={submitHandler}>
+        <input type="text" value={endPoint} onChange={onChangeHandler}/>
+        <button type="submit">Submit</button>
+      </form>
+
+      {container.map((item) => {
+        return (
+          <div>
+            {/* <img src={item.i.imageUrl} alt=""/> */}
+            <p>{item.l}</p>
+          </div>
+        )
+      })}
     </div>
   );
 }
